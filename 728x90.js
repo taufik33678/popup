9150b5ff67a5eeeb6d29e15dfa064f65a6fc649c
taufik33678 <64845779+taufik33678@users.noreply.github.com>
2020-06-15 16:27:04 +0700

    Add files via upload

---
+++ 728x90.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : '57c50c85cdfc1889ccf2bef7b86b6583',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.bestdisplayformats.com/57c50c85cdfc1889ccf2bef7b86b6583/invoke.js"></scr' + 'ipt>');
+</script>
