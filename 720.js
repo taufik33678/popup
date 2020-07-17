a945d3b0ddd724daf85e2d911264bc0cba37bb99
taufik33678 <64845779+taufik33678@users.noreply.github.com>
2020-07-17 22:32:08 +0700

    Add files via upload

---
+++ 720.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : 'ab2bb8c46fd9e8cf2660955c128b1aee',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.topdisplaynetwork.com/ab2bb8c46fd9e8cf2660955c128b1aee/invoke.js"></scr' + 'ipt>');
+</script>
